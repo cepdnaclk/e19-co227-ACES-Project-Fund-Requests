565270595614b16f61da58ecdeb17b7468ec3d24
jali-k <e19210@eng.pdn.ac.lk>
2023-09-06 23:35:46 +0530

    Configure chakra

---
+++ src/components/FormSection1.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@chakra-ui/react";
+
+const FormSection1 = () => {
+  return (
+    <Button colorScheme="blue" onSubmit={() => {}}>
+      Click me
+    </Button>
+  );
+};
+
+export default FormSection1;
