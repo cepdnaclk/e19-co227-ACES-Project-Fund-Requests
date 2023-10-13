1aa13c98b59566dbe1fffe058949794f3123a5b5
Sandamini Madhurasinghe <e19224@eng.pdn.ac.lk>
2023-10-14 00:21:40 +0530

    Admin Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StudentHome from "./Pages/StudentHome";
-import AdminHome from "./Pages/AdminHome";
+import AdminHome1 from "./Pages/AdminHome1";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<StudentHome />} />
-        <Route path="/admin" element={<AdminHome />} />
+        <Route path="/admin" element={<AdminHome1 />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Pages/AdminHome1.tsx
+++ src/Pages/AdminHome1.tsx
@@ -4,14 +4,14 @@ import {
   Text,
   Grid,
   GridItem,
-  Textarea,
+  // Textarea,
   Box,
 
 } from "@chakra-ui/react";
 
-const inputBorderColor = "#97bfd4";
+// const inputBorderColor = "#97bfd4";
 const gridBackgrougndColor = "#F5F5F5";
-const inputFieldTextColor = "black";
+// const inputFieldTextColor = "black";
 const labelColor = "black";
 
 const AdminHome = () => {
