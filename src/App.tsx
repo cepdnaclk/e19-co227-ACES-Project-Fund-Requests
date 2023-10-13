5159397d5d096407845de8b7d6e3858099400997
Jaliya Kumarasiri <e19210@eng.pdn.ac.lk>
2023-10-13 11:07:10 +0530

    Refactor: StudentHome

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AppCopy from "./Pages/StudentHome";
+import StudentHome from "./Pages/StudentHome";
 import AdminHome from "./Pages/AdminHome";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<AppCopy />} />
+        <Route index element={<StudentHome />} />
         <Route path="/admin" element={<AdminHome></AdminHome>} />
       </Routes>
     </BrowserRouter>
