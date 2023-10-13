fdf5602bd2c9355d30aa04926f72a339a1fee77e
Sandamini Madhurasinghe <e19224@eng.pdn.ac.lk>
2023-10-13 22:44:34 +0530

    header added.

--- src/Pages/AdminHome.tsx
+++ src/Pages/AdminHome.tsx
@@ -1,10 +1,35 @@
 import FooterSection from "../components/FooterSection";
+import Header from "../components/Header";
+import {
+  Text,
+  Grid,
+  GridItem,
+  Input,
+  Box,
 
+} from "@chakra-ui/react";
 
 const AdminHome = () => {
   return (
     <>
-      <div>AdminHome</div>
+      <Header></Header>
+      <Box
+      paddingTop={"5%"}
+      paddingBottom={"2%"}
+      // marginX={"10px"}
+      // boxShadow="base"
+      paddingX={"10%"}
+      display={"block"}
+    >
+      <Text
+        color={"#00334E"}
+        fontSize={"20px"}
+        fontWeight={"normal"}
+        paddingBottom={"60px"}
+      >
+        Project “Secure Network Infrastructure Enhancement Project”
+      </Text>
+      </Box>
       <FooterSection></FooterSection>
     </>
   );
