adb1dff888f55003a59e6faffe01ac93bc7d293c
Sandamini Madhurasinghe <e19224@eng.pdn.ac.lk>
2023-10-14 14:28:01 +0530

    AdminHome3 added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StudentHome from "./Pages/StudentHome";
 import AdminHome1 from "./Pages/AdminHome1";
+import AdminHome2 from "./Pages/AdminHome2";
+//import AdminHome3
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<StudentHome />} />
-        <Route path="/admin" element={<AdminHome1 />} />
+        <Route path="/admin1" element={<AdminHome1 />} />
+        <Route path="/admin2" element={<AdminHome2 />} />
+        {/* <Route path="/admin3" element={<AdminHome2 />} /> */}
       </Routes>
     </BrowserRouter>
   );
