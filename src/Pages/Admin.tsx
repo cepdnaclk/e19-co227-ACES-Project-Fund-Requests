fdd63c2da31b5b4112adacf95d1ad40ffd74667e
Sandamini Madhurasinghe <e19224@eng.pdn.ac.lk>
2023-10-17 23:31:58 +0530

    Add more styles

--- src/Pages/Admin.tsx
+++ src/Pages/Admin.tsx
@@ -22,9 +22,6 @@ import { Box,
 //import { Link } from 'react-router-dom';
 //import NextButtonAdmin from "./NextButtonAdmin";
 // import { FaWave } from 'react-icons/fa';
-import cardImage from "../assets/images/cardImage.webp"
-// import ViewMoreButton from "./ViewMoreButton";
-import CardComponent from "./CardComponent";
 
 const Admin = () => {
     return(
@@ -71,69 +68,25 @@ const Admin = () => {
                   }}
                 //   gridTemplateColumns={{ md: "80% 80%" }}
                   gap={4}
-                  marginBottom={0}
-                  fontFamily="Poppins, sans-serif" 
+                  marginBottom={5}
                 >  
                 {/* {{ base: "10px", md: "1%" }} */}
-                <GridItem area={"card1"} colSpan={1} alignItems="center" >
-                    <CardComponent 
-                    cardImage={cardImage} 
-                    bgColor= '#BFD8F8' 
-                    title="Titile" 
-                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam" 
-                    requestDate="17/10/2023"/>
-                </GridItem>
-
-
-                {/* <Card maxW='lg'bg={"#BFD8F8"}>
-                    <CardBody >
+                <GridItem area={"card1"} colSpan={1} alignItems="center">
+                <Card maxW='lg'>
+                    <CardBody>
                         <Image
                         //src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
                         src={cardImage}
                         alt='Green double couch with wooden legs'
                         borderRadius='lg'
                         />
-                        <Stack mt='6' spacing='3' >
-                        <Heading size='md'>Title</Heading>
+                        <Stack mt='6' spacing='3'>
+                        <Heading size='md'>Living room Sofa</Heading>
                         <Text>
-                            Description
+                            This sofa is perfect for modern tropical spaces, baroque inspired
+                            spaces, earthy toned spaces and for people who love a chic design with a
+                            sprinkle of vintage design.
                         </Text>
-                        <Text>
-                            <ViewMoreButton />
-                        </Text>
-
-
-                        {/* <Box
-                            as='button'
-                            height='24px'
-                            lineHeight='1.2'
-                            transition='all 0.2s cubic-bezier(.08,.52,.52,1)'
-                            px='8px'
-                            fontSize='sm'
-                            fontFamily="Poppins, sans-serif"
-                            fontWeight='normal'
-                            color='blue'
-                            position="absolute"
-                            bottom="2"
-                            left="2"
-                            _hover={{ color: 'grey' }}
-                            _active={{
-                                bg: '#dddfe2',
-                                transform: 'scale(0.98)',
-                            }}
-                            >
-                            View More
-                        </Box> */}
-                        {/* <Button 
-                            variant='ghost' 
-                            colorScheme='blue'
-                            fontFamily="Poppins, sans-serif"
-                            fontSize="sm" 
-                            position="absolute"
-                            bottom="2"
-                            left="2">
-                            View More
-                        </Button> */}
                         {/* <Text color='blue.600' fontSize='2xl'>
                             $450
                         </Text> */}
@@ -142,14 +95,14 @@ const Admin = () => {
                         {/* </Stack>
                     </CardBody>
                     {/* <Divider /> */}
-                    {/* <CardFooter fontFamily="Poppins, sans-serif" >
+                    <CardFooter>
                         {/* <ButtonGroup spacing='2'>
                         <Button variant='solid' colorScheme='blue'>
                             Buy now
                         </Button> */}
-                        {/* <Text color="grey" position="absolute" bottom="2" right="2" fontSize="xs">
-                            Request made on ~Date~
-                        </Text>
+                        <Button variant='ghost' colorScheme='blue'>
+                            Add to cart
+                        </Button>
                         {/* </ButtonGroup> */}
                     {/* </CardFooter>
                 </Card> */} 
