00efd546e8fdd6a48dfb1c542975f8a4a48631c1
Sandamini Madhurasinghe <e19224@eng.pdn.ac.lk>
2023-10-18 00:56:22 +0530

    Add more styles to the CardComponents.

--- src/Pages/Admin.tsx
+++ src/Pages/Admin.tsx
@@ -4,25 +4,26 @@
 import Header from "../components/Header";
 import FooterSection from "../components/FooterSection";
 import { Box,
-        Button,
-        ButtonGroup,
-        Card,
-        CardBody,
-        CardFooter,
-        Divider,
+        // Button,
+        // ButtonGroup,
+        // Card,
+        // CardBody,
+        // CardFooter,
+        // Divider,
         Grid,
         GridItem,
-        Heading,
-        Icon,
-        Image,
-        Stack,
+        // Heading,
+        // Icon,
+        // Image,
+        // Stack,
         Text
     } from "@chakra-ui/react";
-import { Link } from 'react-router-dom';
-import NextButtonAdmin from "./NextButtonAdmin";
+//import { Link } from 'react-router-dom';
+//import NextButtonAdmin from "./NextButtonAdmin";
 // import { FaWave } from 'react-icons/fa';
 import cardImage from "../assets/images/cardImage.webp"
-import ViewMoreButton from "./ViewMoreButton";
+// import ViewMoreButton from "./ViewMoreButton";
+import CardComponent from "./CardComponent";
 
 const Admin = () => {
     return(
@@ -68,12 +69,16 @@ const Admin = () => {
                   }}
                 //   gridTemplateColumns={{ md: "80% 80%" }}
                   gap={4}
-                  marginBottom={5}
+                  marginBottom={0}
                   fontFamily="Poppins, sans-serif" 
                 >  
                 {/* {{ base: "10px", md: "1%" }} */}
-                <GridItem area={"card1"} colSpan={1} alignItems="center">
-                <Card maxW='lg'bg={"#BFD8F8"}>
+                <GridItem area={"card1"} colSpan={1} alignItems="center" >
+                    <CardComponent cardImage={cardImage} bgColor= '#BFD8F8' title="Ttile" description="Description" requestDate="17/10/2023"/>
+                </GridItem>
+
+
+                {/* <Card maxW='lg'bg={"#BFD8F8"}>
                     <CardBody >
                         <Image
                         //src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
@@ -127,24 +132,88 @@ const Admin = () => {
                         </Text> */}
 
 
-                        </Stack>
+                        {/* </Stack>
                     </CardBody>
                     {/* <Divider /> */}
-                    <CardFooter fontFamily="Poppins, sans-serif" >
+                    {/* <CardFooter fontFamily="Poppins, sans-serif" >
                         {/* <ButtonGroup spacing='2'>
                         <Button variant='solid' colorScheme='blue'>
                             Buy now
                         </Button> */}
-                        <Text color="grey" position="absolute" bottom="2" right="2" fontSize="xs">
+                        {/* <Text color="grey" position="absolute" bottom="2" right="2" fontSize="xs">
                             Request made on ~Date~
                         </Text>
                         {/* </ButtonGroup> */}
-                    </CardFooter>
-                </Card>
-                </GridItem>
+                    {/* </CardFooter>
+                </Card> */} 
+                
 
                 <GridItem area={"card2"} colSpan={1} alignItems="center">
-                <Card maxW='lg'>
+                <CardComponent cardImage={cardImage} bgColor= '#BFD8F8' title="Ttile" description="Description" requestDate="17/10/2023"/>
+                </GridItem>
+            </Grid>
+                
+            <Box
+            paddingTop={"4%"}
+            paddingBottom={"3%"}
+            // marginX={"10px"}
+            // boxShadow="base"
+            paddingX={"10%"}
+            display={"block"}
+            >
+            <Text
+                    color={"#00334E"}
+                    fontSize={"20px"}
+                    fontWeight={"small"}
+                    paddingBottom={"10px"}
+                    fontFamily={"Poppins, sans-serif"}
+                >
+                    Older Requests
+            </Text>     
+            </Box>   
+            <Grid
+                paddingX = {{base: "10%", md: "10%"}}
+                paddingBottom = {{ base: "10px"}}
+
+                templateAreas={{
+                    base: `"card1"
+                    "card2"`,
+                    md: `"card1 card2"`,
+                  }}
+                  gap={4}
+                  marginBottom={5}
+                  fontFamily="Poppins, sans-serif"   
+            >
+                <GridItem area={"card1"} colSpan={1} alignItems="center" >
+                <CardComponent cardImage={cardImage} bgColor= '#CDCDCD' title="Ttile" description="Description" requestDate="17/10/2023"/>
+                </GridItem>
+                <GridItem area={"card2"} colSpan={1} alignItems="center">
+                <CardComponent cardImage={cardImage} bgColor= '#CDCDCD' title="Ttile" description="Description" requestDate="17/10/2023"/>
+                </GridItem>
+            </Grid>
+
+            <Grid
+                paddingX = {{base: "10%", md: "10%"}}
+                paddingBottom = {{ base: "10px"}}
+
+                templateAreas={{
+                    base: `"card1"
+                    "card2"`,
+                    md: `"card1 card2"`,
+                  }}
+                  gap={4}
+                  marginBottom={5}
+                  fontFamily="Poppins, sans-serif"   
+            >
+                <GridItem area={"card1"} colSpan={1} alignItems="center" >
+                <CardComponent cardImage={cardImage} bgColor= '#CDCDCD' title="Ttile" description="Description" requestDate="17/10/2023"/>
+                </GridItem>
+                <GridItem area={"card2"} colSpan={1} alignItems="center">
+                <CardComponent cardImage={cardImage} bgColor= '#CDCDCD' title="Ttile" description="Description" requestDate="17/10/2023"/>
+                </GridItem>
+            </Grid>
+
+                {/* <Card maxW='lg'>
                     <CardBody>
                         <Image
                         src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
@@ -161,24 +230,25 @@ const Admin = () => {
                         {/* <Text color='blue.600' fontSize='2xl'>
                             $450
                         </Text> */}
-                        </Stack>
-                    </CardBody>
+                        {/* </Stack>
+                    </CardBody> */}
                     {/* <Divider /> */}
-                    <CardFooter>
+                    {/* <CardFooter> */}
                         {/* <ButtonGroup spacing='2'>
                         <Button variant='solid' colorScheme='blue'>
                             Buy now
                         </Button> */}
-                        <Button variant='ghost' colorScheme='blue'>
+                        {/* <Button variant='ghost' colorScheme='blue'>
                             Add to cart
-                        </Button>
+                        </Button> */}
                         {/* </ButtonGroup> */}
-                    </CardFooter>
-                </Card>
-                </GridItem>
-            </Grid>
+                    {/* </CardFooter>
+                </Card>  */}
+
+
+                
             {/* Next Button */}
-            <Grid
+            {/* <Grid
                 paddingX={{ base: "20px", md: "10%" }}
                 paddingY={{ base: "10px", md: "1%" }}
                 >
@@ -187,7 +257,7 @@ const Admin = () => {
                         throw new Error("Function not implemented.");
                     } } />
                 </Link>
-            </Grid>
+            </Grid> */}
             </form>
             <FooterSection></FooterSection>
         </>
